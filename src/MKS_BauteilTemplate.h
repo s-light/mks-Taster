2f96537adb73d4f5512601ef73fae599a20b2fdd
Stefan Krüger <git@s-light.eu>
2024-07-16 22:23:29 +0200

    Initial commit

---
+++ src/MKS_BauteilTemplate.h
@@ -0,0 +1,17 @@
+/*
+    dummy..
+*/
+
+#ifndef MKS_BauteilTemplate_H_
+#define MKS_BauteilTemplate_H_
+
+// include Core Arduino functionality
+#include <Arduino.h>
+
+class MKS_BauteilTemplate {
+
+public:
+    MKS_BauteilTemplate() {}
+};
+
+#endif // MKS_BauteilTemplate_H_
